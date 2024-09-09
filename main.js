33db485ec60d5b2f2fc911b06ef94237b9d87c13
noura <noura.tu19@gmail.com>
2024-09-09 13:53:10 +0300

    edit and make the q clear

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ console.log("Sort the element of the array from smallest to largest");
 console.log(numbers.sort((a, b) => a - b));
 
 //2 
- console.log(" square of the numbers");
+ console.log("find  square of the numbers :");
 let square = numbers.map((num) => num * num);
 console.log("Square of the numbers" + square);
 
 //3 
-console.log(" return the numbers greater than 25");
+console.log(" return the numbers greater than 25 :");
 let greater = numbers.filter((num) => num > 25);
-console.log("Numbers greater than 25 " + greater);
+console.log(" The are Numbers: " + greater);
 // 4
-console.log("check if the array contains a number 3");
+console.log("check if the array contains a number 3 ? ");
 let check = numbers.includes(3);
 console.log("Array contains number 3 : " + check);
 
@@ -25,12 +25,12 @@ let filter1 = numbers.filter((num) => num % 2 === 0);
 console.log("Filter the even numbers :" + filter1);
 
 //6
-console.log(" remve two middle elements from the array");
+console.log(" remove two middle elements from the array");
 let remove = numbers.splice(3, 2);
 console.log("Array after removing two middle elements " + numbers);
 
 //7
-console.log(" multiply all array elements");
+console.log(" multiply all array elements ?");
 let multiply = numbers.reduce((acc, num) => acc * num);
 console.log("Multiplication of all array elements " + multiply);
 
@@ -59,12 +59,12 @@ let names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
 //1
 console.log(" return the emlement of array that contain the letter 'a'");
 let contain = names.filter((name) => name.includes('a'));
-console.log("Elements of array that contain the letter 'a' are " + contain);
+console.log("Elements of array that contain the letter 'a' are  : " + contain);
 
 //2
-console.log("trun the arrray into a string");
-let string = names.join();
-console.log("Array as a string " + string);
+console.log("trun the arrray into a string ?");
+let string = names.join( " ");
+console.log("Array as a string : " + string);
 // ------------------ 
 
 // q3
@@ -74,10 +74,10 @@ console.log("  chseck if the array contain element start with 'b' ");
 let start = fruits.map((fruit) => fruit.startsWith('b'));
 console.log("Elements of array that start with 'b' are " + start);
 
-console.log("  print new array after removing the the sceond element until the fifth element");
+ 
 let remove1 = fruits.splice(2, 3);
 console.log("Array after removing the second element until the fifth element " + fruits);
- console.log("  add yellow to the front of the array");
+ 
 let add = fruits.unshift('yellow');
 console.log("Array after adding yellow to the front " + fruits);
 
@@ -86,7 +86,7 @@ console.log("Array after adding yellow to the front " + fruits);
 //1
 let words = ['hello', 'world', 'javascript', 'array', 'function'];
 
-console.log("Create a new array containing the length of each word in the original array");
+ 
 let length = words.map((word) => word.length);
 console.log("Length of each word in the original array " + length);
 //2
